36d544b1c292e877f1471bdaf299b464fbc3756f
Tynka <martyna05p@gmail.com>
2025-11-14 11:11:46 +0100

    II.2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Logger logger=new LoggerFile("data_a.log");
         String path="/Users/tynka/IdeaProjects/as1dataseries/inputData/";
         //odczytywanie pliku a
         BufferedReader data= new BufferedReader(new FileReader(path+"data_a1.txt"));
@@ -16,8 +17,11 @@ public class Main {
         }
         System.out.println(getOutputInfo(liczby, 0, "Task I.1", "data_a1.txt"));
         data.close();
+        logger.flush();
+
 
         //odczytywanie pliku b
+        Logger logger2=new LoggerFile("data_b.log");
         BufferedReader data2= new BufferedReader(new FileReader(path+"data_b1.txt"));
         String line2;
         ArrayList<Readout> liczby2=new ArrayList<>();
@@ -28,12 +32,15 @@ public class Main {
             }
             catch(NumberFormatException e){
                 noOfInvalidRecords+=1;
+                logger2.log(Logger.Level.ERROR, "Faulty record in [data_b1.txt]: " + line2);
             }
         }
         System.out.println(getOutputInfo(liczby2, noOfInvalidRecords, "Task I.2", "data_b1.txt"));
         data2.close();
+        logger2.flush();
 
         //odczytywanie pliku c
+        Logger logger3=new LoggerFile("data_c.log");
         BufferedReader data3= new BufferedReader(new FileReader(path+"data_c1.txt"));
         String line3;
         ArrayList<Readout> liczby3=new ArrayList<>();
@@ -48,11 +55,12 @@ public class Main {
             }
             catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
                 noOfInvalidRecords3+=1;
+                logger3.log(Logger.Level.ERROR, "Faulty record in [data_c1.txt]: " + line3);
             }
         }
-
         System.out.println(getOutputInfo(liczby3, noOfInvalidRecords3, "Task II.1", "data_c1.txt"));
         data3.close();
+        logger3.flush();
     }
     //wartość maksymalna
     static Readout getMax(ArrayList<Readout> data){
@@ -74,14 +82,17 @@ public class Main {
         return mean;
     }
     //mediana - dla wszystkich przypadków (pamiętając, ze indeksy ArrayList są od 0
-    static double getMedian(ArrayList<Readout> data){
+    static MedianWrapper getMedian(ArrayList<Readout> data){
         Collections.sort(data);
         int n = data.size();
         if(n%2==0){
-            return (data.get(n/2-1).getValue()+(data.get(n/2)).getValue())/2;
+            Readout elem1=data.get(n/2-1);
+            Readout elem2=data.get(n/2);
+            return new MedianWrapper(elem1, elem2);
         }
         else {
-            return data.get(n/2).getValue();
+            Readout elem=data.get(n/2);
+            return new MedianWrapper(elem);
         }
     }
     //elementy centralne
@@ -99,7 +110,7 @@ public class Main {
     static String getOutputInfo(ArrayList<Readout> data, int noOfInvalidRecords, String title, String filename){
         String str="";
         String separator= "\n------------------------------\n";
-        str = str + title + "\nMartyna Pieczka, 297955" + separator + "Data filename: " + filename + "\nLength of the series: " + data.size() + "\nMax value: " +getMax(data).toString() + "\nMin value: " + getMin(data).toString() + String.format("\nMean value: %.3f", getMean(data)) + String.format("\nMedian %.3f", getMedian(data)) + "\nNumber of central elements: " + noOfCentralElements(data);
+        str = str + title + "\nMartyna Pieczka, 297955" + separator + "Data filename: " + filename + "\nLength of the series: " + data.size() + "\nMax value: " +getMax(data).toString() + "\nMin value: " + getMin(data).toString() + String.format("\nMean value: %.3f", getMean(data)) + "\nMedian: " + getMedian(data).toString() + "\nNumber of central elements: " + noOfCentralElements(data);
         if(noOfInvalidRecords==0) {
             str += separator;
             return str;
