db231561b56f01dbc9670f90413eb66593d5bc54
Tynka <martyna05p@gmail.com>
2025-12-10 15:34:55 +0100

    III.3 2

--- src/IOHelper.java
+++ src/IOHelper.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 public class IOHelper {
     //odczytywanie plików Task III.1
     static FileContent readFile(String filename, Logger logger) throws IOException {
-        Sensor dummySensor=new Sensor("<N/A>");
 
         String path = "inputData/";
         BufferedReader data = new BufferedReader(new FileReader(path + filename));
@@ -20,12 +19,12 @@ public class IOHelper {
             try {
                 String[] parts = line.split(" ");
                 if (parts.length ==1){
-                    dummySensor.addReadout(new Readout(Double.parseDouble(line)));
+                    addReadoutToSensor(sensors, "<N/A>", new Readout(Double.parseDouble(line)));
                 }
                 else if (parts.length==2){
                     double value = Double.parseDouble(parts[0]);
                     String uuid = parts[1];
-                    dummySensor.addReadout(new ReadoutWithUuid(value, uuid));
+                    addReadoutToSensor(sensors, "<N/A>", new ReadoutWithUuid(value, uuid));
                 }
                 else{
                     double value = Double.parseDouble(parts[0]);
@@ -59,19 +58,20 @@ public class IOHelper {
     //string końcowy
     static String getOutputInfo(FileContent fContent, String title) {
         String str = "";
-        String separator = "\n------------------------------\n";
+        String separatorLong = "\n------------------------------";
+        String separatorShort="\n-------";
 
         int noOfInvalidRecords = fContent.getNoOfInvalidRecords();
+
         for(Sensor sensor: fContent.getSensors()){
             String filename = fContent.getFileName();
 
-            str = str + title + "\nMartyna Pieczka, 297955" + separator + "Data filename: " + filename +
+            str = str + title + "\nMartyna Pieczka, 297955" + separatorLong + "\nData filename: " + filename+separatorShort + "\nSensor name: "+ sensor.getName() +
                     "\nLength of the series: " + sensor.getLengthOfData() + "\nMax value: " + sensor.getMax() +
                     "\nMin value: " + sensor.getMin().toString() + String.format("\nMean value: %.3f", sensor.getMean())
                     + "\nMedian: " + sensor.getMedian() + "\nNumber of central elements: " + sensor.noOfCentralElements();
-
         }
-        str = str + "\nNumber of invalid records: " + noOfInvalidRecords + separator;
+        str = str + "\nNumber of invalid records: " + noOfInvalidRecords + separatorLong;
         return str;
     }
 }
