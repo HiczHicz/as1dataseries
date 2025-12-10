8a2a8c523167dc6918beaca09c163aef5c50d4b7
Tynka <martyna05p@gmail.com>
2025-12-10 13:53:24 +0100

    III.2 8-12

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.*;
 
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -15,54 +17,6 @@ public class Main {
         processOneFile("data_c1.txt", "data_c.log", "Task II.1");
     }
 
-    //wartość maksymalna
-    static Readout getMax(ArrayList<Readout> data) {
-        Collections.sort(data);
-        return data.get(data.size() - 1);
-    }
-
-    //wartość minimalna
-    static Readout getMin(ArrayList<Readout> data) {
-        Collections.sort(data);
-        return data.get(0);
-    }
-
-    //średnia
-    static double getMean(ArrayList<Readout> data) {
-        double mean = 0;
-        for (int i = 0; i < data.size(); i++) {
-            mean += data.get(i).getValue();
-        }
-        mean = mean / data.size();
-        return mean;
-    }
-
-    //mediana
-    static MedianWrapper getMedian(ArrayList<Readout> data) {
-        Collections.sort(data);
-        int n = data.size();
-        if (n % 2 == 0) {
-            Readout elem1 = data.get(n / 2 - 1);
-            Readout elem2 = data.get(n / 2);
-            return new MedianWrapper(elem1, elem2);
-        } else {
-            Readout elem = data.get(n / 2);
-            return new MedianWrapper(elem);
-        }
-    }
-
-    //elementy centralne
-    static int noOfCentralElements(ArrayList<Readout> data) {
-        double epsilon = (getMax(data).getValue() - getMin(data).getValue()) / 100;
-        int numberOfCentralEl = 0;
-        for (int i = 0; i < data.size(); i++) {
-            if ((getMean(data) - data.get(i).getValue() < epsilon) && (data.get(i).getValue() - getMean(data) < epsilon)) {
-                numberOfCentralEl++;
-            }
-        }
-        return numberOfCentralEl;
-    }
-
     //string końcowy
     static String getOutputInfo(FileContent fContent, String title) {
         String str = "";
@@ -70,13 +24,12 @@ public class Main {
 
         int noOfInvalidRecords = fContent.getNoOfInvalidRecords();
         for(Sensor sensor: fContent.getSensors()){
-            ArrayList<Readout> data = sensor.getData();
             String filename = fContent.getFileName();
 
             str = str + title + "\nMartyna Pieczka, 297955" + separator + "Data filename: " + filename +
-                    "\nLength of the series: " + data.size() + "\nMax value: " + getMax(data).toString() +
-                    "\nMin value: " + getMin(data).toString() + String.format("\nMean value: %.3f", getMean(data))
-                    + "\nMedian: " + getMedian(data) + "\nNumber of central elements: " + noOfCentralElements(data);
+                    "\nLength of the series: " + sensor.getLengthOfData() + "\nMax value: " + sensor.getMax() +
+                    "\nMin value: " + sensor.getMin().toString() + String.format("\nMean value: %.3f", sensor.getMean())
+                    + "\nMedian: " + sensor.getMedian() + "\nNumber of central elements: " + sensor.noOfCentralElements();
 
         }
         str = str + "\nNumber of invalid records: " + noOfInvalidRecords + separator;
--- src/Sensor.java
+++ src/Sensor.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sensor {
     private String name;
@@ -13,7 +14,57 @@ public class Sensor {
     public void addReadout(Readout readout){
         data.add(readout);
     }
-    public ArrayList<Readout> getData(){
-        return data;
+    // size
+    public int getLengthOfData(){
+        return data.size();
+    }
+
+    //wartość maksymalna
+    public Readout getMax() {
+        Collections.sort(data);
+        int size=data.size()-1;
+        return data.get(size);
+    }
+
+    //wartość minimalna
+    public Readout getMin() {
+        Collections.sort(data);
+        return data.get(0);
+    }
+
+    //średnia
+    public double getMean() {
+        double mean = 0;
+        for (int i = 0; i < data.size(); i++) {
+            mean += data.get(i).getValue();
+        }
+        mean = mean / data.size();
+        return mean;
+    }
+
+    //mediana
+    public MedianWrapper getMedian() {
+        Collections.sort(data);
+        int n = data.size();
+        if (n % 2 == 0) {
+            Readout elem1 = data.get(n / 2 - 1);
+            Readout elem2 = data.get(n / 2);
+            return new MedianWrapper(elem1, elem2);
+        } else {
+            Readout elem = data.get(n / 2);
+            return new MedianWrapper(elem);
+        }
+    }
+
+    //elementy centralne
+    public int noOfCentralElements() {
+        double epsilon = (getMax().getValue() - getMin().getValue()) / 100;
+        int numberOfCentralEl = 0;
+        for (int i = 0; i < data.size(); i++) {
+            if ((getMean() - data.get(i).getValue() < epsilon) && (data.get(i).getValue() - getMean() < epsilon)) {
+                numberOfCentralEl++;
+            }
+        }
+        return numberOfCentralEl;
     }
  }
